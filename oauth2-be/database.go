8c3e1d51a0d8b4602a5b81034c4d73e9dcbb8bb5
zeim839 <zeim839@users.noreply.github.com>
2023-11-02 10:53:57 -0400

    oauth2-be: implemented signin, signup, verify, grant routes

--- oauth2-be/database.go
+++ oauth2-be/database.go
@@ -44,7 +44,7 @@ func (db *Database) Stop() error {
 }
 
 // ReadUser attempts to return a user by their email address.
-func (db *Database) ReadUser(email string) (UserModel, error) {
+func (db *Database) ReadUserByEmail(email string) (UserModel, error) {
 	if db.stopped {
 		return UserModel{}, fmt.Errorf("db has been stopped")
 	}
@@ -53,6 +53,97 @@ func (db *Database) ReadUser(email string) (UserModel, error) {
 	err := coll.FindOne(context.TODO(), bson.D{{"email", email}}).Decode(&user)
 	if err != nil {
 		return UserModel{}, err
+	}
+
+	return user, nil
+}
+
+func (db *Database) ReadUser(id string) (UserModel, error) {
+	if db.stopped {
+		return UserModel{}, fmt.Errorf("db has stopped")
+	}
+
+	objectId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return UserModel{}, err
+	}
+
+	var user UserModel
+	coll := db.client.Database(db.dbname).Collection("users")
+	err = coll.FindOne(context.TODO(), bson.D{{"_id", objectId}}).Decode(&user)
+	if err != nil {
+		return UserModel{}, err
+	}
+
+	return user, nil
+}
+
+func (db *Database) UpdateUser(user UserModel) (int64, error) {
+	if db.stopped {
+		return 0, fmt.Errorf("db has been stopped")
+	}
+
+	objectId, err := primitive.ObjectIDFromHex(user.ID)
+	if err != nil {
+		return 0, err
+	}
+
+	user.ID = ""
+
+	coll := db.client.Database(db.dbname).Collection("users")
+	res, err := coll.UpdateOne(context.TODO(), bson.D{{"_id", objectId}},
+		bson.D{{"$set", user}})
+
+	if err != nil {
+		return 0, err
+	}
+
+	return res.ModifiedCount, nil
+}
+
+func (db *Database) CreateUser(user UserModel) (string, error) {
+	if db.stopped {
+		return "", fmt.Errorf("db has stopped")
+	}
+
+	coll := db.client.Database(db.dbname).Collection("users")
+	res, err := coll.InsertOne(context.TODO(), user)
+	if err != nil {
+		return "", err
+	}
+
+	return res.InsertedID.(primitive.ObjectID).Hex(), nil
+}
+
+func (db *Database) ReadPendingUser(id string) (PendingUserModel, error) {
+	if db.stopped {
+		return PendingUserModel{}, fmt.Errorf("db has been stopped")
+	}
+
+	objectId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return PendingUserModel{}, err
+	}
+
+	var user PendingUserModel
+	coll := db.client.Database(db.dbname).Collection("pending_users")
+	err = coll.FindOne(context.TODO(), bson.D{{"_id", objectId}}).Decode(&user)
+	if err != nil {
+		return PendingUserModel{}, err
+	}
+
+	return user, nil
+}
+
+func (db *Database) ReadPendingUserByEmail(email string) (PendingUserModel, error) {
+	if db.stopped {
+		return PendingUserModel{}, fmt.Errorf("db has been stopped")
+	}
+	var user PendingUserModel
+	coll := db.client.Database(db.dbname).Collection("pending_users")
+	err := coll.FindOne(context.TODO(), bson.D{{"email", email}}).Decode(&user)
+	if err != nil {
+		return PendingUserModel{}, err
 	}
 
 	return user, nil
@@ -69,10 +160,106 @@ func (db *Database) CreatePendingUser(usr PendingUserModel) (string, error) {
 		return "", err
 	}
 
-	oid, ok := res.InsertedID.(primitive.ObjectID)
-	if !ok {
-		return "", fmt.Errorf("internal server error")
+	return res.InsertedID.(primitive.ObjectID).Hex(), nil
+}
+
+func (db *Database) DeletePendingUser(id string) error {
+	if db.stopped {
+		return fmt.Errorf("db has stopped")
 	}
 
-	return oid.String(), nil
+	objectId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+
+	coll := db.client.Database(db.dbname).Collection("pending_users")
+	_, err = coll.DeleteOne(context.TODO(), bson.D{{"_id", objectId}})
+
+	return err
+}
+
+func (db *Database) CreateVerifyEmailSignin(verif VerifyEmailSigninModel) (string, error) {
+	if db.stopped {
+		return "", fmt.Errorf("db has been stopped")
+	}
+
+	coll := db.client.Database(db.dbname).Collection("verify_email_signin")
+	res, err := coll.InsertOne(context.TODO(), verif)
+	if err != nil {
+		return "", err
+	}
+
+	return res.InsertedID.(primitive.ObjectID).Hex(), nil
+}
+
+func (db *Database) ReadVerifyEmailSigninByEmail(email string) (VerifyEmailSigninModel, error) {
+	if db.stopped {
+		return VerifyEmailSigninModel{}, fmt.Errorf("db has been stopped")
+	}
+	var verif VerifyEmailSigninModel
+	coll := db.client.Database(db.dbname).Collection("verify_email_signin")
+	err := coll.FindOne(context.TODO(), bson.D{{"email", email}}).Decode(&verif)
+	if err != nil {
+		return VerifyEmailSigninModel{}, err
+	}
+
+	return verif, nil
+}
+
+func (db *Database) ReadVerifyEmailSignin(id string) (VerifyEmailSigninModel, error) {
+	if db.stopped {
+		return VerifyEmailSigninModel{}, fmt.Errorf("db has been stopped")
+	}
+
+	objectId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return VerifyEmailSigninModel{}, err
+	}
+
+	var verif VerifyEmailSigninModel
+	coll := db.client.Database(db.dbname).Collection("verify_email_signin")
+	err = coll.FindOne(context.TODO(), bson.D{{"_id", objectId}}).Decode(&verif)
+	if err != nil {
+		return VerifyEmailSigninModel{}, err
+	}
+
+	return verif, err
+}
+
+func (db *Database) DeleteVerifyEmailSignin(id string) error {
+	if db.stopped {
+		return fmt.Errorf("db has been stopped")
+	}
+
+	objectId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return err
+	}
+
+	coll := db.client.Database(db.dbname).Collection("verify_email_signin")
+	_, err = coll.DeleteOne(context.TODO(), bson.D{{"_id", objectId}})
+
+	return err
+}
+
+func (db *Database) ReadClient(id string) (ClientModel, error) {
+	if db.stopped {
+		return ClientModel{}, fmt.Errorf("db has stopped")
+	}
+
+	objectId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return ClientModel{}, err
+	}
+
+	var client ClientModel
+	coll := db.client.Database(db.dbname).Collection("clients")
+	err = coll.FindOne(context.TODO(), bson.D{{"_id", objectId}}).Decode(&client)
+
+	if err != nil {
+		return ClientModel{}, err
+	}
+
+	return client, nil
 }
--- oauth2-be/errors.go
+++ oauth2-be/errors.go
@@ -3,6 +3,7 @@ package main
 import "github.com/gin-gonic/gin"
 
 // TODO: Make sure there's no duplicates. Return UI-friendly messages.
+// Error names should be consistent (e.g. use 'invalid' in place of 'bad').
 
 var (
 	ErrMissingFields = gin.H{
@@ -57,86 +58,126 @@ var (
 
 	ErrIncorrectUserPass = gin.H{
 		"code": "430",
-		"msg": "incorrect username or password",
+		"msg":  "incorrect username or password",
 	}
 
 	ErrNewJWT = gin.H{
 		"code": "431",
-		"msg": "internal server error",
+		"msg":  "internal server error",
 	}
 
 	ErrMalformedURLParams = gin.H{
 		"code": "432",
-		"msg": "malformed or missing required URL parameters",
+		"msg":  "malformed or missing required URL parameters",
 	}
 
 	ErrGrantTokenNotFound = gin.H{
 		"code": "435",
-		"msg": "grant token is invalid or expired",
+		"msg":  "grant token is invalid or expired",
 	}
 
 	ErrGrantWrongClient = gin.H{
 		"code": "436",
-		"msg": "client redirect_uri or client_id is incorrect",
+		"msg":  "client redirect_uri or client_id is incorrect",
 	}
 
 	ErrCreateAccessToken = gin.H{
 		"code": "437",
-		"msg": "internal server error",
+		"msg":  "internal server error",
 	}
 
 	ErrCreateRefreshToken = gin.H{
 		"code": "438",
-		"msg": "internal server error",
+		"msg":  "internal server error",
 	}
 
 	ErrInvalidRefreshToken = gin.H{
 		"code": "439",
-		"msg": "invalid or revoked refresh token",
+		"msg":  "invalid or revoked refresh token",
 	}
 
 	ErrRefreshWrongClient = gin.H{
 		"code": "440",
-		"msg": "refresh token client and authorization client don't match",
+		"msg":  "refresh token client and authorization client don't match",
 	}
 
 	ErrInvalidRealm = gin.H{
 		"code": "441",
-		"msg": "you are not authorized to access this route",
+		"msg":  "you are not authorized to access this route",
 	}
 
 	ErrInvalidClientType = gin.H{
 		"code": "442",
-		"msg": "invalid client type, expected 'confidential' or 'public'",
+		"msg":  "invalid client type, expected 'confidential' or 'public'",
 	}
 
 	ErrInvalidPublicScope = gin.H{
 		"code": "443",
-		"msg": "invalid scope for public client",
+		"msg":  "invalid scope for public client",
 	}
 
 	ErrClientNameTaken = gin.H{
 		"code": "444",
-		"msg": "client name has already been registered",
+		"msg":  "client name has already been registered",
 	}
 
 	ErrInvalidRedirectURI = gin.H{
 		"code": "445",
-		"msg": "invalid redirect_uri",
+		"msg":  "invalid redirect_uri",
 	}
 
 	ErrRedirectURITaken = gin.H{
 		"code": "446",
-		"msg": "redirect_uri has already been registered",
+		"msg":  "redirect_uri has already been registered",
 	}
 
 	ErrInvalidClientInfo = gin.H{
 		"code": "447",
-		"msg": "client name and description must be alphanumeric",
+		"msg":  "client name and description must be alphanumeric",
 	}
 
 	ErrHashError = gin.H{
 		"code": "448",
-		"msg": "internal server error",
+		"msg":  "internal server error",
+	}
+
+	ErrVerifyEmail = gin.H{
+		"code": "449",
+		"msg":  "please check your email address for a verification email. Email addresses must be verified at least once per semester to maintain student status",
+	}
+
+	ErrEmailAlreadySent = gin.H{
+		"code": "450",
+		"msg":  "verification email already sent. Please check your inbox & spam folders",
+	}
+
+	ErrInvalidURLParam = gin.H{
+		"code": "451",
+		"msg":  "invalid URL parameters",
+	}
+
+	ErrNoCookie = gin.H{
+		"code": "452",
+		"msg": "could not extract auth cookie",
+	}
+
+	ErrUserNotFound = gin.H{
+		"code": "453",
+		"msg": "could not find user",
+	}
+
+	ErrPasswordChanged = gin.H{
+		"code": "454",
+		"msg": "password changed",
+	}
+
+	ErrVerificationRequired = gin.H{
+		"code": "455",
+		"msg": "email verification required. please log in again",
+	}
+
+	ErrClientNotFound = gin.H{
+		"code": "456",
+		"msg": "client id not found",
 	}
 )
