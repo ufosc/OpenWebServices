ba5a45ebb8a5049f293807d6a50c119168667b6f
zeim839 <zeim839@users.noreply.github.com>
2023-11-02 10:52:52 -0400

    oauth2-be: added Go mod documentation

--- oauth2-be/validation.go
+++ oauth2-be/validation.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"golang.org/x/crypto/bcrypt"
 	"net/mail"
 	"strings"
 )
 
 const alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const numeric = "0123456789"
 const special = "`~!@#$%^&*()_-=+[{]}\\|:;\"',<.>/"
+const numeric = "0123456789"
 
+// ValidateEmail checks whether email is a valid email address ending
+// with the @ufl.edu domain.
 func ValidateEmail(email string) bool {
 	if _, err := mail.ParseAddress(email); err != nil {
 		return false
@@ -26,6 +29,8 @@ func ValidateEmail(email string) bool {
 	return true
 }
 
+// ValidatePassword checks whether password is at least 12 chars, contains
+// both digits and characters, and has at least 1 special character.
 func ValidatePassword(password string) string {
 	// Must be a minimum of 12 characters.
 	if len(password) < 12 {
@@ -49,6 +54,56 @@ func ValidatePassword(password string) string {
 	return ""
 }
 
+// VerifyPassword verifies that a given password matches the hash.
 func VerifyPassword(hash, password string) bool {
+	if err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)); err != nil {
+		return false
+	}
 	return true
+}
+
+func validateTokenScope(scope []string) bool {
+	// Token-based clients only have access to 'public' scope.
+	if len(scope) > 1 {
+		return false
+	}
+
+	if scope[0] != "public" {
+		return false
+	}
+
+	return true
+}
+
+func validateCodeScope(scope []string) bool {
+	if len(scope) > 2 {
+		return false
+	}
+
+	// Must be either 'public' or 'email'
+	for _, v := range scope {
+		if v != "public" && v != "email" {
+			return false
+		}
+	}
+
+	return true
+}
+
+// ValidateScope checks whether the scope string is valid for the given response
+// type.
+func ValidateScope(resType string, scope []string) bool {
+	if len(scope) == 0 {
+		return false
+	}
+
+	if resType == "token" {
+		return validateTokenScope(scope)
+	}
+
+	if resType == "code" {
+		return validateCodeScope(scope)
+	}
+
+	return false
 }
