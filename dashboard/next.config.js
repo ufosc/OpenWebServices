59be907cb4ceedd150482b06d2e64f8a3a319625
zeim839 <zeim839@users.noreply.github.com>
2023-11-23 21:52:27 -0500

    dashboard: next.js proxy

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -1,6 +1,47 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export'
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/auth/authorize',
+        destination: '/authorize',
+      },
+      {
+        source: '/auth/authorize',
+        destination: '/authorize',
+      },
+      {
+        source: '/auth/signup',
+        destination: 'http://localhost:8080/auth/signup',
+      },
+      {
+        source: '/auth/signin',
+        destination: 'http://localhost:8080/auth/signin',
+      },
+      {
+        source: '/auth/signout',
+        destination: 'http://localhost:8080/auth/signout',
+      },
+      {
+        source: '/auth/verify',
+        destination: 'http://localhost:8080/auth/verify',
+      },
+      {
+        source: '/auth/grant',
+        destination: 'http://localhost:8080/auth/grant',
+      },
+      {
+        source: '/auth/token',
+        destination: 'http://localhost:8080/auth/token',
+      },
+      {
+        source: '/client/:id',
+        destination: 'http://localhost:8080/client/:id',
+      },
+      {
+        source: '/client',
+        destination: 'http://localhost:8080/client',
+      },
+    ]
+  },
 }
-
-module.exports = nextConfig
