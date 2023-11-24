96aa135f215c6e3032ac3e6d9485ddd1b0517356
zeim839 <zeim839@users.noreply.github.com>
2023-11-23 21:49:20 -0500

    dashboard: create components

--- dashboard/components/ImageBanner/imagebanner.tsx
+++ dashboard/components/ImageBanner/imagebanner.tsx
@@ -1,8 +1,13 @@
-import './imagebanner.scss'
+'use client'
+
+import './style.scss'
+import { RandContext } from '@/app/context'
+import { useContext } from 'react'
 
 const decorationImage = [
   "/dalle01.png", "/dalle02.png", "/dalle03.png",
-  "/dalle04.png", "/dalle05.png",
+  "/dalle04.png", "/dalle05.png", "/dalle06.png",
+  "/dalle07.png"
 ]
 
 const decorationText = [
@@ -10,19 +15,21 @@ const decorationText = [
   "Impressionist painting of UF building (AI Generated)",
   "Impressionist painting of UF building (AI Generated)",
   "Impressionist painting of UF stadium (AI Generated)",
-  "Impressionist painting of Century Tower (AI Generated)"
+  "Impressionist painting of Century Tower (AI Generated)",
+  "Picture of Gator enjoying the view (AI Generated)",
+  "Photo of Miniature Gator Skateboarder (AI Generated)",
 ]
 
 const ImageBanner = () => {
-  const random = Math.floor(Math.random() * decorationImage.length)
-  const text = decorationText[random]
+  const random = useContext(RandContext)
   const src = decorationImage[random]
+  const txt = decorationText[random]
   return (
     <div className="decoration">
       <div style={{overflow: "hidden"}}>
 	  <img src={src} id="decoration--image"/>
       </div>
-      <div id="decoration--meta">{text}</div>
+      <div id="decoration--meta">{txt}</div>
     </div>
   )
 }
