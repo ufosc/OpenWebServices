68a7cfcb0ce099c6dc3c425e758af826638c2150
zeim839 <zeim839@users.noreply.github.com>
2023-11-23 22:00:43 -0500

    dashboard: created APIController

---
+++ dashboard/APIController/types.ts
@@ -0,0 +1,43 @@
+export type TypeSigninBody = {
+  email: string;
+  password: string;
+}
+
+export type TypeSignupBody = {
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+  captcha: string;
+}
+
+export interface APIResponse {
+  message?: string;
+  error?: string;
+}
+
+export interface TypeGetClientResponse {
+  id: string;
+  description: string;
+  message: string;
+  name: string;
+  redirect_uri: string;
+  response_type: string;
+  scope: Array<string>;
+}
+
+export interface TypeAuthGrant {
+  response_type: string | null;
+  client_id: string | null;
+  redirect_uri: string | null;
+  scope: string | null;
+  state: string | null;
+}
+
+export interface TypeClient {
+  id: string;
+  name: string;
+  redirect_uri: string;
+  response_type: string;
+  scope: Array<string>;
+}
