ea1d72d9999fc45b82521f89f0df44beaddf909d
zeim839 <zeim839@users.noreply.github.com>
2023-12-19 10:08:56 +0400

    dashboard: optimize image loading

--- dashboard/components/ImageBanner/imagebanner.tsx
+++ dashboard/components/ImageBanner/imagebanner.tsx
@@ -2,12 +2,21 @@
 
 import './style.scss'
 import { RandContext } from '@/app/context'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
+import Image from 'next/image'
+import { Loading } from '@carbon/react'
+
+import dalle01 from "@/public/dalle01.png"
+import dalle02 from "@/public/dalle02.png"
+import dalle03 from "@/public/dalle03.png"
+import dalle04 from "@/public/dalle04.png"
+import dalle05 from "@/public/dalle05.png"
+import dalle06 from "@/public/dalle06.png"
+import dalle07 from "@/public/dalle07.png"
 
 const decorationImage = [
-  "/dalle01.png", "/dalle02.png", "/dalle03.png",
-  "/dalle04.png", "/dalle05.png", "/dalle06.png",
-  "/dalle07.png"
+  dalle01, dalle02, dalle03, dalle04,
+  dalle05, dalle06, dalle07,
 ]
 
 const decorationText = [
@@ -21,13 +30,20 @@ const decorationText = [
 ]
 
 const ImageBanner = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true)
   const random = useContext(RandContext)
   const src = decorationImage[random]
   const txt = decorationText[random]
   return (
     <div className="decoration">
       <div style={{overflow: "hidden"}}>
-	  <img src={src} id="decoration--image"/>
+	{
+	  (isLoading) ?
+	    (<Loading id="decoration--loading" withOverlay={false} />)
+	    : null
+	}
+	<Image src={src} id="decoration--image" alt={txt}
+	  onLoad={() => { setIsLoading(false) }}/>
       </div>
       <div id="decoration--meta">{txt}</div>
     </div>
