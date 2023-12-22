50b6b5b65adce507c4ed05936b0df65fc24b1c87
zeim839 <zeim839@users.noreply.github.com>
2023-12-22 09:42:58 +0400

    dashboard: refactor landing page

--- dashboard/app/page.tsx
+++ dashboard/app/page.tsx
@@ -1,9 +1,13 @@
 'use client'
 
+import { useState } from 'react'
 import { redirect } from 'next/navigation'
 import { useCookies } from 'next-client-cookies'
-import MyAccount from '@/components/MyAccount/MyAccount'
-import { Heading, Tabs, TabList, Tab, TabPanels, TabPanel } from '@carbon/react'
+import MyAccount from '@/components/MyAccount'
+import { GetUser, IsAPISuccess } from '@/APIController/API'
+import Users from '@/components/Users'
+import { Heading, Tabs, TabList, Tab,
+  TabPanels, TabPanel, Loading } from '@carbon/react'
 
 export default function Page() {
   const cookies = useCookies()
@@ -12,25 +16,38 @@ export default function Page() {
     redirect("/authorize")
   }
 
+  const [user, setUser] = useState<Object | null>(null)
+  if (user === null) {
+    GetUser(jwt).then((res) => {
+      if (!IsAPISuccess(res)) {
+	cookies.remove('ows-jwt')
+	location.replace("/authorize")
+	return
+      }
+      setUser(res)
+    }).catch((err) => {
+      cookies.remove('ows-jwt')
+      location.replace("/authorize")
+    })
+    return (<Loading withOverlay={true} />)
+  }
+
   return (
     <div className="account">
       <Heading> Dashboard </Heading>
       <Tabs>
-	<TabList contained className="account--tablist">
-	  <Tab>Analytics</Tab>
-	  <Tab>Clients</Tab>
-	  <Tab>Users</Tab>
+	<TabList contained className="account--tablist" aria-label="dashboard">
+	  <Tab>My Account</Tab>
+	  <Tab disabled={!user.realms?.includes("clients.read")}>Clients</Tab>
+	  <Tab disabled={!user.realms?.includes("users.read")}>Users</Tab>
+	  <Tab disabled>Analytics</Tab>
 	  <Tab disabled>DNS</Tab>
 	  <Tab disabled>CDN</Tab>
-	  <Tab>My Account</Tab>
 	</TabList>
 	<TabPanels>
+	  <TabPanel><MyAccount user={user} /></TabPanel>
 	  <TabPanel> Tab Panel 1 </TabPanel>
 	  <TabPanel> Tab Panel 2 </TabPanel>
-	  <TabPanel> Tab Panel 3 </TabPanel>
-	  <TabPanel> Tab Panel 4 </TabPanel>
-	  <TabPanel> Tab Panel 5 </TabPanel>
-	  <TabPanel><MyAccount /></TabPanel>
 	</TabPanels>
       </Tabs>
     </div>
